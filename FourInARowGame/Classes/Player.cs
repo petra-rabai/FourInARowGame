6e3042dc207a21a9e0b5b604caea26c549aeae72
Petra Rábai <petra.rabai@icloud.com>
2022-02-12 16:56:27 +0100

    .

---
+++ FourInARowGame/Classes/Player.cs
@@ -0,0 +1,29 @@
+﻿using FourInARowGame.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourInARowGame.Classes
+{
+    public class Player : IPlayer
+    {
+        public string choosedDiscColor { get; set; }
+        public string choosedMenu { get; set; }
+        public string choosedGameMode { get; set; }
+        public string consoleData { get; set; }
+
+        public string GetChoosedMenu()
+        {
+            ReadFromConsole();
+            choosedMenu = consoleData;
+            return choosedMenu;
+        }
+
+        public void ReadFromConsole()
+        {
+            consoleData = Console.ReadLine();
+        }
+    }
+}
